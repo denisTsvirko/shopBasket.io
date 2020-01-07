2e7be38354226fdd366fe49683cf5ea8958a62b5
denisTsvirko <30652220+denisTsvirko@users.noreply.github.com>
2020-01-07 15:04:26 +0300

    Fixed code smells

--- install/index.php
+++ install/index.php
@@ -1,39 +1,39 @@
 <?php
+
 require_once 'mysqlconnect.php';
+
 header('Content-Type: text/html; charset=utf-8');
 
 $dbh = new PDO($dsn, $user, $password);
 
-function pdoSet($allowed, &$values, $source) {
+function pdoSet($allowed, &$values, $source) 
+{
     $set = '';
-    $values = array();
+    $values = [];
     foreach ($allowed as $field) {
         if (isset($source[$field])) {
-            $set.="`".str_replace("`","``",$field)."`". "=:$field, ";
+            $set .= "`" . str_replace("`", "``", $field) . "`" . "=:$field, ";
             $values[$field] = $source[$field];
         }
     }
     return substr($set, 0, -2);
 }
 
-if(isset($_POST['all'])){
-
+if(isset($_POST['all'])) {
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     $data = $dbh->query('SELECT * FROM Products');
 
     echo json_encode($data->fetchAll());
 }
 
-if(isset($_POST['basket'])){
+if(isset($_POST['basket'])) {
     $obj = json_decode($_POST['basket']);
 
     for ($i=0; $i < count($obj); $i++) {
-
-        $allowed = array("id_Product","quantity"); // allowed fields
+        $allowed = array("id_Product", "quantity");
         $source = array('id_Product' => $obj[$i]->id, 'quantity' => $obj[$i]->quantity);
 
-        $sql = "INSERT INTO Buy SET ".pdoSet($allowed,$values,$source);
+        $sql = "INSERT INTO Buy SET " . pdoSet($allowed, $values, $source);
         $stm = $dbh->prepare($sql);
         $stm->execute($values);
     }
